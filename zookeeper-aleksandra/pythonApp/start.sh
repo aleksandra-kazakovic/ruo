aafb0fccd6543b7c6fd076beb3a8273c55087c47
aleksandra-kazakovic <102159297+aleksandra-kazakovic@users.noreply.github.com>
2022-07-11 16:39:33 +0200

    Add files via upload

---
+++ zookeeper-aleksandra/pythonApp/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+path=`pwd`
+mysql_init_path="$path/mysql/init"
+mysql_storage_path="$path/mysql/storage"
+docker run --name=my-mysql \
+    --env="MYSQL_ROOT_PASSWORD=kazak" \
+    -p 3306:3306 \
+    -v mysql_init_path:/docker-entrypoint-initdb.d  \
+    -v mysql_storage_path:/var/lib/mysql \
+    -d  \
+    mysql:latest
+
