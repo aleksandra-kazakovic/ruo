1347b0974a08e2e2ab790994f04e35d557c8134d
aleksandra-kazakovic <102159297+aleksandra-kazakovic@users.noreply.github.com>
2022-07-13 20:52:44 +0200

    Delete zookeeper-aleksandra directory

--- zookeeper-aleksandra/pythonApp/modelMl.py
+++
@@ -1,9 +0,0 @@
-
-class ModelMl:
-  def __init__(self, id, model_name, guid, path, accuracy, auc):
-    self.id = id
-    self.modelName = model_name
-    self.guid = guid
-    self.modelPath = path
-    self.accuracy = accuracy
-    self.auc = auc
